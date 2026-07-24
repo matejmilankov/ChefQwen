a5fb876988a653f979b14272268d59d282c06858
Matej Milankov <matej.milankov@gmail.com>
2026-07-25 01:55:48 +0200

    Added switch button for themes

--- src/components/ThemeMode.jsx
+++ src/components/ThemeMode.jsx
@@ -1,10 +1,18 @@
 export function ThemeMode(props) {
+  const toggleMode = () => {
+    props.setIsDarkMode(prevMode => !prevMode);
+  };
 
-
-    return (
-        <div className="themes">
-            <button onClick={ () => {props.setIsDarkMode(true);} }>Dark mode</button>
-            <button onClick={ () => {props.setIsDarkMode(false);} }>Light mode</button>
-        </div>
-    );
+  return (
+    <div className="themes">
+      <div 
+        onClick={toggleMode} 
+        className={`themes-relative ${props.isDarkMode ? 'active' : ''}`}
+      >
+        <div className={`theme-label ${!props.isDarkMode ? 'active' : ''}`}>L</div>
+        <div className={`theme-label ${props.isDarkMode ? 'active' : ''}`}>D</div>
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
 }
